4b85b53f8d3b83aaab595304737d1a63018338c5
Pathum Goonathilake <pathumveyron24@gmail.com>
2021-06-28 17:51:08 +0530

    Auth Server Files Added

---
+++ Auth_Server/auth_server/auth_server/config.py
@@ -0,0 +1,12 @@
+# Settings and Configurations
+
+# DATABASE CONFIGURATIONS
+DB_ENGINE = 'django.db.backends.postgresql'
+DB_NAME = 'test_auth_server'
+DB_USERNAME = 'postgres'
+DB_PASSWORD = '123'
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
+
+# TIME ZONE
+TIME_ZONE = 'Asia/Colombo'
